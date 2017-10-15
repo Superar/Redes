5d0151b781e814d2a02dfb4be7fdb02026791bf9
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-10-15 02:15:18 -0200

    Transformação de tab em espaço e correções de estilo

--- trabalhos/T1/cgi-bin/webserver.py
+++ trabalhos/T1/cgi-bin/webserver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import cgi, cgitb 
+import cgi
+import cgitb
 import subprocess
 import re
 
@@ -10,39 +11,39 @@ executaveis = ['ps', 'df', 'finger', 'uptime']
 exec_list = list()
 
 try:
-	form = cgi.FieldStorage()
+    form = cgi.FieldStorage()
 
-	for executavel in executaveis:
-		comandos = re.findall('maq[1-3]_' + executavel + '#', '#'.join(form))
-		for elem in comandos:
-			exec_list.append(elem[:-1].split('_'))
+    for executavel in executaveis:
+        comandos = re.findall('maq[1-3]_' + executavel + '#', '#'.join(form))
+        for elem in comandos:
+            exec_list.append(elem[:-1].split('_'))
 
-	for prog in exec_list:
-		result.append(prog[0] + ' executando ' + prog[1])
-		args = [prog[1]]
+    for prog in exec_list:
+        result.append(prog[0] + ' executando ' + prog[1])
+        args = [prog[1]]
 
-		if prog[0] + '_' + prog[1] + '_args' in form:
-			for tok in form.getvalue(prog[0] + '_' + prog[1] + '_args').split(' '):
-				args.append(tok)	
+        if prog[0] + '_' + prog[1] + '_args' in form:
+            for tok in form.getvalue(prog[0] + '_' + prog[1] + '_args').split(' '):
+                args.append(tok)
 
-		p = subprocess.Popen(args, stdout=subprocess.PIPE)
-		for s in p.stdout.readlines():
-			result.append(s)
-		p.stdout.close()
+        p = subprocess.Popen(args, stdout=subprocess.PIPE)
+        for s in p.stdout.readlines():
+            result.append(s)
+        p.stdout.close()
 
 except KeyError:
-	print 'Content-type:text/html'
-	print
-	print "<html><body> ERROR </body></html>"
+    print 'Content-type:text/html'
+    print
+    print '<html><body> ERROR </body></html>'
 
 else:
-	print 'Content-type:text/html'
-	print
-	print '<html>'
-	if len(result) > 0: 	
-		for s in result:
-			print '<h1>%s</h1>' % s
-	else:
-		print '<h1>Selecione algo</h1>'
-	
-	print '</html>'
+    print 'Content-type:text/html'
+    print
+    print '<html>'
+    if len(result) > 0:
+        for s in result:
+            print '<h1>%s</h1>' % s
+    else:
+        print '<h1>Selecione algo</h1>'
+
+    print '</html>'
