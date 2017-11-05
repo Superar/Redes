e6e34e90070f7d149912b2a0640dd9e99d2ef2f7
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-11-04 19:24:47 -0700

    Correção nas conexões pós-merge

--- trabalhos/T1/cgi-bin/backend.py
+++ trabalhos/T1/cgi-bin/backend.py
@@ -23,33 +23,36 @@ if [x for x in exclude_list if x in commands_str]:
     print 'Argumentos invalidos: ' + str(exclude_list)
     raise ValueError
 
-for maq in maq_ports:
+for maq in sorted(maq_ports.keys()):
     regex = '(?:' + maq + r')\S+(?=\n)'
     exec_list = re.findall(regex, commands_str)
     exec_list = [c.split('#') for c in exec_list]
     
     if exec_list:
         print '<h1>' + maq + '</h1>'
-        try: 
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             sock.connect(('localhost', maq_ports[maq]))
         except Exception as ex:
             print 'backend.py'
             print ex
+        else:
+            for cmd in exec_list:       
+                print '<h2>' + cmd[1] + '</h2>'
+                msg = Message()
+                msg.request('127.0.0.2', cmd[1:], 1)
+                data = msg.encode()
+                data.seek(0)
 
-        for cmd in exec_list:       
-            print '<h2>' + cmd[1] + '</h2>'
-            msg = Message()
-            msg.request('127.0.0.2', cmd[1:], 1)
-            data = msg.encode()
-            data.seek(0)
+                sock.sendall(data.read())
+                
+                data_response = sock.recv(1024)
+                buffer = io.BytesIO(data_response)
+                response = Message()
+                response.decode(buffer)
 
-            sock.sendall(data.read())
-            
-            data_response = sock.recv(1024)
-            buffer = io.BytesIO(data_response)
-            response = Message()
-            response.decode(buffer)
-
-            print '<pre>' + response.content + '</pre>'
-        
-       # sock.shutdown(socket.SHUT_RDWR)
+                print '<pre>' + response.content + '</pre>'
+        finally:
+            sock.shutdown(socket.SHUT_WR)
+            sock.close()
